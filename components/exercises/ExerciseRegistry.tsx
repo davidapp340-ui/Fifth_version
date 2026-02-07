dfafc1ffea54928665ca25c9da9ee3ed498a16dd
davidapp340-ui <davidapp340@gmail.com>
2026-02-07 21:17:56 +0200

    Added EXERCISES_SUMMARY.md

--- components/exercises/ExerciseRegistry.tsx
+++ components/exercises/ExerciseRegistry.tsx
@@ -9,9 +9,36 @@
 import React from 'react';
 import { View, Text, StyleSheet } from 'react-native';
 
-// Import exercise animation components here as they're created
 import PalmingExercise from './palming_exercise';
-// import BallTrackingAnimation from './BallTrackingAnimation';
+import Exercise02 from './Exercise02';
+import Exercise03 from './Exercise03';
+import Exercise04 from './Exercise04';
+import Exercise05 from './Exercise05';
+import Exercise06 from './Exercise06';
+import Exercise07 from './Exercise07';
+import Exercise08 from './Exercise08';
+import Exercise09 from './Exercise09';
+import Exercise10 from './Exercise10';
+import Exercise11 from './Exercise11';
+import Exercise12 from './Exercise12';
+import Exercise13 from './Exercise13';
+import Exercise14 from './Exercise14';
+import Exercise15 from './Exercise15';
+import Exercise16 from './Exercise16';
+import Exercise17 from './Exercise17';
+import Exercise18 from './Exercise18';
+import Exercise19 from './Exercise19';
+import Exercise20 from './Exercise20';
+import Exercise21 from './Exercise21';
+import Exercise22 from './Exercise22';
+import Exercise23 from './Exercise23';
+import Exercise24 from './Exercise24';
+import Exercise25 from './Exercise25';
+import Exercise26 from './Exercise26';
+import Exercise27 from './Exercise27';
+import Exercise28 from './Exercise28';
+import Exercise29 from './Exercise29';
+import Exercise30 from './Exercise30';
 
 // Placeholder component for exercises not yet implemented
 const PlaceholderExercise: React.FC<{ animationId: string }> = ({ animationId }) => (
@@ -43,14 +70,37 @@ const styles = StyleSheet.create({
   },
 });
 
-/**
- * Registry mapping animation_id to React Native components
- */
 export const ExerciseRegistry: Record<string, React.ComponentType<any>> = {
-  // Map animation IDs to their components
   'palming_v1': PalmingExercise,
-  // 'ball_tracking_v1': BallTrackingAnimation,
-  // Add more as they're created
+  'ex_02': Exercise02,
+  'ex_03': Exercise03,
+  'ex_04': Exercise04,
+  'ex_05': Exercise05,
+  'ex_06': Exercise06,
+  'ex_07': Exercise07,
+  'ex_08': Exercise08,
+  'ex_09': Exercise09,
+  'ex_10': Exercise10,
+  'ex_11': Exercise11,
+  'ex_12': Exercise12,
+  'ex_13': Exercise13,
+  'ex_14': Exercise14,
+  'ex_15': Exercise15,
+  'ex_16': Exercise16,
+  'ex_17': Exercise17,
+  'ex_18': Exercise18,
+  'ex_19': Exercise19,
+  'ex_20': Exercise20,
+  'ex_21': Exercise21,
+  'ex_22': Exercise22,
+  'ex_23': Exercise23,
+  'ex_24': Exercise24,
+  'ex_25': Exercise25,
+  'ex_26': Exercise26,
+  'ex_27': Exercise27,
+  'ex_28': Exercise28,
+  'ex_29': Exercise29,
+  'ex_30': Exercise30,
 };
 
 /**
